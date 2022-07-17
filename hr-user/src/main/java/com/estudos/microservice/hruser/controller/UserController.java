9e448e296cff03217d3e87e1f889b54e4429fd65
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2022-07-17 20:15:16 -0300

    Feat: implementa o servidor hr-user

---
+++ hr-user/src/main/java/com/estudos/microservice/hruser/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.estudos.microservice.hruser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.microservice.hruser.entity.User;
+import com.estudos.microservice.hruser.repository.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findByEmail(@RequestParam String email) {
+		User obj = repository.findByEmail(email);
+		return ResponseEntity.ok(obj);
+	}
+}
---
+++ hr-user/src/main/java/com/estudos/microservice/hruser/entity/package-info.java
@@ -0,0 +1,1 @@
+package com.estudos.microservice.hruser.entity;
