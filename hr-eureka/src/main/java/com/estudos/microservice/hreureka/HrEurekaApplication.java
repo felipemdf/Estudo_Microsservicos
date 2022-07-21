3266a21a48a33ec4a537cea064652c36981a5a91
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2022-07-20 21:53:21 -0300

    Feat: adicionado o servidor eureka e realizado a configuração inicial

---
+++ hr-eureka/src/main/java/com/estudos/microservice/hreureka/HrEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.estudos.microservice.hreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HrEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaApplication.class, args);
+	}
+
+}
